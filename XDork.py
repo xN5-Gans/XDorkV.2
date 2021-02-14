418f853b561cb54452682cc778327e6201c5c734
xN5-Gans <77638633+xN5-Gans@users.noreply.github.com>
2021-02-14 17:51:06 +0700

    Create XDork.py

---
+++ XDork.py
@@ -0,0 +1,35 @@
+
+ 
+import os
+import time
+
+os.system('clear')
+#xN5
+def U():
+#Xploiter
+#Recode Izin Bgst pyzluclufer@gmail.com
+	print("\033[4m__        _______ _     ____ ___  __  __ _____
+\ \      / / ____| |   / ___/ _ \|  \/  | ____|
+ \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|
+  \ V  V / | |___| |__| |__| |_| | |  | | |___
+   \_/\_/  |_____|_____\____\___/|_|  |_|_____|")
+	time.sleep(2)
+	print("")
+	print("\33[31;1m┌ｘＮ５ Ｘｐｌｏｉｔｅｒ╼")
+	time.sleep(2)
+	print("")
+	print('\33[33;1mNOTE┌Jika Eror Berarti Terlalu Banyak Web╼')
+	print("")
+	time.sleep(2)
+	print('\33[31;1mSorry Jika Masih Sering Ngebug')
+	print("")
+	time.sleep(1)
+	print('\33[32;1mHAPPY HACKING...')
+	print("")
+	time.sleep(2)
+U()
+from googlesearch import search
+query = str(input("\33[0;36m┌DORKNYA~>"))
+os.system('clear')
+for URL in search(query):
+  print(URL)
